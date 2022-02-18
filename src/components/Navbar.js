60e09915f9ab7a600aeb961357415fa0f9c157e8
Mohammad Ferdiyanzah <mferdiyanzah@gmail.com>
2022-02-18 16:20:43 +0700

    refactor

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,8 @@ import logo from '../assets/img/home/logo-petrolida-desktop.svg'
 import hamb from "../assets/img/home/hamb-nav.svg"
 import exit from "../assets/img/home/exit-nav.svg"
 
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
   const [toggleNavbar, setToggleNavbar] = useState(false)
   const [navbarImg, setNavbarImg] = useState(true)
@@ -18,15 +20,15 @@ const Navbar = () => {
   return (
     <nav>
       <div className="container d-flex align-items-center justify-content-between">
-        <a href="/" id="logo-petrolida">
-          <img src={logo} alt="Logo Petrolida" />
-        </a>
+        <Link to="/" id="logo-petrolida">
+          <img src={logo} className="logo" alt="Logo Petrolida" />
+        </Link>
         <ul className={`d-flex animate__animated ${toggleNavbar && 'responsive animate__slideInLeft'}`} id="list-link-nav">
-          <li><a href="">About</a></li>
-          <li><a href="">Events</a></li>
-          <li><a href="">FAQ</a></li>
-          <li><a href="">Contact</a></li>
-          <li><a href="" className="sign-in">Sign in</a></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/events">Events</Link></li>
+          <li><Link to="/faq">FAQ</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/users/sign_in" className="sign-in">Sign in</Link></li>
         </ul>
         <button className="d-block d-md-none btn shadow-none" id="btn-nav" onClick={handleResponsive}><img src={navbarImg ? hamb : exit} alt="Button Navigation" id="img-btn-nav"/></button>
       </div>
